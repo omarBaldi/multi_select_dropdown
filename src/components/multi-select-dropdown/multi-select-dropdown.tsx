c109b19cd4d4c7a3b0e389590cdfa42537d29d3d
Omar Baldi <omarbaldidk@gmail.com>
2022-09-24 14:25:34 +0200

    Applied state to open/close dropdown and conditionally render the options

--- src/components/multi-select-dropdown/multi-select-dropdown.tsx
+++ src/components/multi-select-dropdown/multi-select-dropdown.tsx
@@ -1,28 +1,51 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import MultiSelectDropdownProps from './dto';
 
+/**
+ *
+ * TODO: add scoped style for component (using styled components)
+ * TODO: implement theme (using styled components)
+ */
 const MultiSelectDropdown: FC<MultiSelectDropdownProps> = ({
   placeholderLabel = 'Choose an option',
   options,
 }: MultiSelectDropdownProps) => {
+  const [openDropdown, setOpenDropdown] = useState<boolean>(false);
+
+  const toggleDrodpown = () => setOpenDropdown((prevState) => !prevState);
+
   return (
-    <div>
+    <div style={{ border: '1px solid', position: 'relative' }}>
       <div
+        onClick={toggleDrodpown}
         style={{
           display: 'flex',
           justifyContent: 'space-between',
           alignItems: 'center',
+          padding: '0 0.5rem',
+          cursor: 'pointer',
         }}
       >
         <p>{placeholderLabel}</p>
         <button>.</button>
       </div>
 
-      <div>
-        {options.map(({ id, label }) => (
-          <div key={`option-${id}`}>{label}</div>
-        ))}
-      </div>
+      {openDropdown && (
+        <div
+          style={{
+            border: '1px solid red',
+            position: 'absolute',
+            left: 0,
+            right: 0,
+          }}
+        >
+          {options.map(({ id, label }) => (
+            <div key={`option-${id}`} style={{ padding: '1rem 0.5rem' }}>
+              {label}
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
